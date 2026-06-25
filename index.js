1bce7921fc85066fd593e415f4d3c5f1261c6b31
6gzygf9c7z-netizen <6gzygf9c7z@privaterelay.appleid.com>
2026-06-25 07:12:59 +0100

    Initial TNN Bot Setup

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-index.js
+const { Client, GatewayIntentBits } = require('discord.js');
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
+  ]
+});
+
+client.once('ready', () => {
+  console.log(`TNN Bot is online as ${client.user.tag}`);
+});
+
+client.on('messageCreate', message => {
+  if (message.author.bot) return;
+
+  if (message.content.toLowerCase() === 'hello') {
+    message.reply('TNN Bot reporting live... sharp sharp! 📰');
+  }
+});
+
+client.login(process.env.TOKEN);
